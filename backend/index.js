9662a33d3d5fd45dec7d954fee8fd6496c0420c5
Zzyzzx2 <rahanshankar38@gmail.com>
2023-11-23 13:26:25 +0530

    Modified index backend-4

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,23 @@ import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import cors from "cors";
-
+import "dotenv/config.";
 const PORT = process.env.PORT || 5555;
-const mongoDBURL = process.env.MONGODB_URL;
+const mongoDBURL = process.env.MONGODBURL;
 const app = express();
 
 //Middleware for parsing data
 app.use(express.json());
 
 //Option 1: Allow All Origins w/ default of cors(*)
-//app.use(cors());
+app.use(cors());
 //Option 2: Use custom headers
-app.use(
-  cors({
-    origin: "https://article-management-frontend.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://article-management-frontend.vercel.app",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 //GET Method for HomePage
 app.get("/", (request, response) => {
