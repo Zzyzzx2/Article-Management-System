0b7fb905d1b27b1f8f3ec283626a5055f11fbcbd
Zzyzzx2 <rahanshankar38@gmail.com>
2023-12-15 22:32:06 +0530

    Fixed url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // export const backendUrl = "http://localhost:5555";
-export const backendUrl = "https://https://article-management-api.vercel.app";
+export const backendUrl = "https://article-management-api.vercel.app";
